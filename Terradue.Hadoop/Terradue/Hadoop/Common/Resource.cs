1fee9f2ff13177262a440c12ba6dd293d00efc42
eboissier <eboissier@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-10-21 14:44:38 +0000

    git-svn-id: https://gateway.terradue.com/repo/products/sugar/Terradue.Hadoop/trunk@38680 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

---
+++ Terradue.Hadoop/Terradue/Hadoop/Common/Resource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Text;
+
+namespace Terradue.Hadoop
+{
+    public abstract class Resource
+    {
+        public JsonObject Info { get; set; }
+    }
+
+    public enum Version
+    {
+        V1
+    }
+
+    //public enum TypeOfJob
+    //{
+    //    Hive,
+    //    Pig,
+    //    MapReduce
+    //}
+}
